f1ae2847f88f1f872f2ba74da7e421e286fe88f9
m166116 <Daniels.James@mayo.edu>
2018-05-16 18:04:26 -0500

    Nested components groundwork.

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component'
 
 @NgModule({
   declarations: [ // modules we make are added here.
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [ // modules from other sources are added here
     BrowserModule,
